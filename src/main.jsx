3c84d881b9b4c7a202ab3a79d11d11a456ea54e5
yassin <meddyassin@hotmail.com>
2023-05-01 17:43:01 +0100

    ..

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,6 @@ import App from "./App";
 import "./index.css";
 import { BrowserRouter as Router } from "react-router-dom";
 
-console.log("API Key:", process.env.API_KEY);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
